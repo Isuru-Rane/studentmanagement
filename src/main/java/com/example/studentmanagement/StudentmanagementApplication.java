7d170e53c8bb7b0bc547a827d7f3e17b332fee8e
Isuru <>
2023-07-18 10:09:18 +0530

    first Commit

--- src/main/java/com/example/studentmanagement/StudentmanagementApplication.java
+++ src/main/java/com/example/studentmanagement/StudentmanagementApplication.java
@@ -8,6 +8,9 @@ public class StudentmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementApplication.class, args);
+
+		System.out.println("Spring Project");
+
 	}
 
 }
